5a9a7421181b103b0619b9e031f5310b8a26a308
NguyenMinhKha1410 <minhkha20190@gmail.com>
2026-01-03 17:03:34 +0700

    CuoiKy1MindX

--- TestCuoiKymindx/vaccines.py
+++ TestCuoiKymindx/vaccines.py
@@ -24,18 +24,12 @@ def get_vaccines():
         engine='python',
         on_bad_lines='skip'
     )
-
     # ép kiểu object để pandas không tự xuất NaN đặc biệt
     for c in df.columns:
         df[c] = df[c].astype(object)
-
     # thay mọi NaN/NaT/inf bằng None
     df = df.where(pd.notnull(df), None)
-
     vaccines_list = df.to_dict(orient='records')
-    print(">>> USING vaccine_app.py WITH NaN->None <<<")
     return jsonify(vaccines_list)
-
-
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
